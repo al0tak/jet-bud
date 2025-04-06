b63d2f27d80f7baf6de4264251fd40e7829be4c4
al0tak <artemtrubin.letters@gmail.com>
2025-04-06 15:46:11 +0200

    Default tab structure created

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,20 @@
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{ title: "Home", headerShown: false }}
+      />
+      <Tabs.Screen
+        name="accounts"
+        options={{ title: "Accounts", headerShown: false }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{ title: "Settings", headerShown: false }}
+      />
+    </Tabs>
+  );
+}
